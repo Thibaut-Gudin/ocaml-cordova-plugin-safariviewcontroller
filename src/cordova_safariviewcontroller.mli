0e525d6a312ff7434f47d4417d11513535f39375
Thibaut <tgudin@besport.com>
2021-08-06 18:11:23 +0200

    adding show and is_available functions into the library

--- src/cordova_safariviewcontroller.mli
+++ src/cordova_safariviewcontroller.mli
@@ -1,1 +1,21 @@
-(*TODO*)
+val is_available : (unit -> unit) -> unit
+  [@@js.global "SafariViewController.isAvailable"]
+
+type options
+
+val options :
+  ?url:string ->
+  ?hidden:bool ->
+  ?animated:bool ->
+  ?transition:string ->
+  ?enter_reader_mode_if_available:bool ->
+  (*TODO imposser que les string ci-dessous soi des ref de couleur et pas n'importe quel string, si possible?*)
+  ?tint_color:string ->
+  ?bar_color:string ->
+  ?control_tint_color:string ->
+  unit ->
+  options
+  [@@js.builder] [@@js.verbatim_names]
+
+val show : options -> onSuccess:(unit -> unit) -> onError:(unit -> unit) -> unit
+  [@@js.global "SafariViewController.show"]
